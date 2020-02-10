3bdd14a46bcb8848d39ec490f1b539a848fb1ffa
Shinya Tsuda <shinya@dacci.org>
2020-02-10 23:50:21 +0900

    Added JSON serializers.

---
+++ src/main/java/org/dacci/junk/util/CloudFormationModule.java
@@ -0,0 +1,322 @@
+package org.dacci.junk.util;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import java.io.IOException;
+import org.dacci.junk.util.cfn.Base64;
+import org.dacci.junk.util.cfn.Cidr;
+import org.dacci.junk.util.cfn.Conditional;
+import org.dacci.junk.util.cfn.FindInMap;
+import org.dacci.junk.util.cfn.GetAZs;
+import org.dacci.junk.util.cfn.GetAtt;
+import org.dacci.junk.util.cfn.ImportValue;
+import org.dacci.junk.util.cfn.Join;
+import org.dacci.junk.util.cfn.Ref;
+import org.dacci.junk.util.cfn.Select;
+import org.dacci.junk.util.cfn.Split;
+import org.dacci.junk.util.cfn.Sub;
+import org.dacci.junk.util.cfn.Transform;
+
+@SuppressWarnings("serial")
+public class CloudFormationModule extends SimpleModule {
+  private static class Base64Serializer extends StdSerializer<Base64> {
+    public Base64Serializer() {
+      this(null);
+    }
+
+    public Base64Serializer(Class<Base64> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Base64 value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeObjectField("Fn::Base64", value.getValue());
+      gen.writeEndObject();
+    }
+  }
+
+  private static class CidrSerializer extends StdSerializer<Cidr> {
+    public CidrSerializer() {
+      this(null);
+    }
+
+    public CidrSerializer(Class<Cidr> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Cidr value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeArrayFieldStart("Fn::Cidr");
+      gen.writeObject(value.getIpBlock());
+      gen.writeNumber(value.getCount());
+      gen.writeNumber(value.getCidrBits());
+      gen.writeEndArray();
+      gen.writeEndObject();
+    }
+  }
+
+  private static class ConditionalSerializer extends StdSerializer<Conditional> {
+    public ConditionalSerializer() {
+      this(null);
+    }
+
+    public ConditionalSerializer(Class<Conditional> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Conditional value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeArrayFieldStart("Fn::" + value.getClass().getSimpleName());
+
+      for (var condition : value.getConditions()) {
+        gen.writeObject(condition);
+      }
+
+      gen.writeEndArray();
+      gen.writeEndObject();
+    }
+  }
+
+  private static class FindInMapSerializer extends StdSerializer<FindInMap> {
+    public FindInMapSerializer() {
+      this(null);
+    }
+
+    public FindInMapSerializer(Class<FindInMap> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(FindInMap value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeArrayFieldStart("Fn::FindInMap");
+      gen.writeObject(value.getMapName());
+      gen.writeObject(value.getTopLevelKey());
+      gen.writeObject(value.getSecondLevelKey());
+      gen.writeEndArray();
+      gen.writeEndObject();
+    }
+  }
+
+  private static class GetAttSerializer extends StdSerializer<GetAtt> {
+    public GetAttSerializer() {
+      this(null);
+    }
+
+    public GetAttSerializer(Class<GetAtt> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(GetAtt value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeArrayFieldStart("Fn::GetAtt");
+      gen.writeObject(value.getResource());
+      gen.writeObject(value.getAttribute());
+      gen.writeEndArray();
+      gen.writeEndObject();
+    }
+  }
+
+  private static class GetAZsSerializer extends StdSerializer<GetAZs> {
+    public GetAZsSerializer() {
+      this(null);
+    }
+
+    public GetAZsSerializer(Class<GetAZs> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(GetAZs value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeObjectField("Fn::GetAZs", value.getRegion());
+      gen.writeEndObject();
+    }
+  }
+
+  private static class ImportValueSerializer extends StdSerializer<ImportValue> {
+    public ImportValueSerializer() {
+      this(null);
+    }
+
+    public ImportValueSerializer(Class<ImportValue> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(ImportValue value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeObjectField("Fn::ImportValue", value.getReference());
+      gen.writeEndObject();
+    }
+  }
+
+  private static class JoinSerializer extends StdSerializer<Join> {
+    public JoinSerializer() {
+      this(null);
+    }
+
+    public JoinSerializer(Class<Join> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Join value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeArrayFieldStart("Fn::Join");
+      gen.writeObject(value.getDelimiter());
+      gen.writeObject(value.getValues());
+      gen.writeEndArray();
+      gen.writeEndObject();
+    }
+  }
+
+  private static class SelectSerializer extends StdSerializer<Select> {
+    public SelectSerializer() {
+      this(null);
+    }
+
+    public SelectSerializer(Class<Select> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Select value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeArrayFieldStart("Fn::Select");
+      gen.writeNumber(value.getIndex());
+      gen.writeObject(value.getSelection());
+      gen.writeEndArray();
+      gen.writeEndObject();
+    }
+  }
+
+  private static class SplitSerializer extends StdSerializer<Split> {
+    public SplitSerializer() {
+      this(null);
+    }
+
+    public SplitSerializer(Class<Split> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Split value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeArrayFieldStart("Fn::Split");
+      gen.writeObject(value.getDelimiter());
+      gen.writeObject(value.getSource());
+      gen.writeEndArray();
+      gen.writeEndObject();
+    }
+  }
+
+  private static class SubSerializer extends StdSerializer<Sub> {
+    public SubSerializer() {
+      this(null);
+    }
+
+    public SubSerializer(Class<Sub> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Sub value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      var hasVariables = value.getVariables() != null && !value.getVariables().isEmpty();
+
+      gen.writeStartObject();
+      gen.writeFieldName("Fn::Sub");
+
+      if (hasVariables) gen.writeStartArray();
+
+      gen.writeObject(value.getTemplate());
+
+      if (hasVariables) {
+        gen.writeObject(value.getVariables());
+        gen.writeEndArray();
+      }
+
+      gen.writeEndObject();
+    }
+  }
+
+  private static class TransformSerializer extends StdSerializer<Transform> {
+    public TransformSerializer() {
+      this(null);
+    }
+
+    public TransformSerializer(Class<Transform> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Transform value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeObjectFieldStart("Fn::Transform");
+      gen.writeObjectField("Name", value.getName());
+      gen.writeObjectField("Parameters", value.getParameters());
+      gen.writeEndObject();
+      gen.writeEndObject();
+    }
+  }
+
+  private static class RefSerializer extends StdSerializer<Ref> {
+    public RefSerializer() {
+      this(null);
+    }
+
+    public RefSerializer(Class<Ref> t) {
+      super(t);
+    }
+
+    @Override
+    public void serialize(Ref value, JsonGenerator gen, SerializerProvider provider)
+        throws IOException {
+      gen.writeStartObject();
+      gen.writeObjectField("Ref", value.getReference());
+      gen.writeEndObject();
+    }
+  }
+
+  private static class Holder {
+    public static final CloudFormationModule INSTANCE = new CloudFormationModule();
+  }
+
+  public static CloudFormationModule getInstance() {
+    return Holder.INSTANCE;
+  }
+
+  private CloudFormationModule() {
+    addSerializer(Base64.class, new Base64Serializer());
+    addSerializer(Cidr.class, new CidrSerializer());
+    addSerializer(Conditional.class, new ConditionalSerializer());
+    addSerializer(FindInMap.class, new FindInMapSerializer());
+    addSerializer(GetAtt.class, new GetAttSerializer());
+    addSerializer(GetAZs.class, new GetAZsSerializer());
+    addSerializer(ImportValue.class, new ImportValueSerializer());
+    addSerializer(Join.class, new JoinSerializer());
+    addSerializer(Select.class, new SelectSerializer());
+    addSerializer(Split.class, new SplitSerializer());
+    addSerializer(Sub.class, new SubSerializer());
+    addSerializer(Transform.class, new TransformSerializer());
+    addSerializer(Ref.class, new RefSerializer());
+  }
+}
---
+++ src/test/java/org/dacci/junk/util/CloudFormationModuleTest.java
@@ -0,0 +1,134 @@
+package org.dacci.junk.util;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import org.dacci.junk.util.cfn.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CloudFormationModuleTest {
+  private static ObjectMapper json;
+
+  @BeforeAll
+  public static void setUpClass() {
+    json = new ObjectMapper();
+    json.registerModule(CloudFormationModule.getInstance());
+  }
+
+  @Test
+  public void testBase64() throws JsonProcessingException {
+    var data = new Base64("valueToEncode");
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::Base64\":\"valueToEncode\"}"));
+  }
+
+  @Test
+  public void testCidr() throws JsonProcessingException {
+    var data = new Cidr("192.168.0.0/24", 6, 5);
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::Cidr\":[\"192.168.0.0/24\",6,5]}"));
+  }
+
+  @ParameterizedTest
+  @ValueSource(classes = {And.class, Equals.class, If.class, Not.class, Or.class})
+  public void testConditional(Class<? extends Conditional> clazz)
+      throws ReflectiveOperationException, JsonProcessingException {
+    var constructor = clazz.getConstructor(Object[].class);
+    var data = constructor.newInstance(new Object[] {new Object[] {"a", "b"}});
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::" + clazz.getSimpleName() + "\":[\"a\",\"b\"]}"));
+  }
+
+  @Test
+  public void testFindInMap() throws JsonProcessingException {
+    var data = new FindInMap("MapName", "TopLevelKey", "SecondLevelKey");
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::FindInMap\":[\"MapName\",\"TopLevelKey\",\"SecondLevelKey\"]}"));
+  }
+
+  @Test
+  public void testGetAtt() throws JsonProcessingException {
+    var data = new GetAtt("myELB", "DNSName");
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::GetAtt\":[\"myELB\",\"DNSName\"]}"));
+  }
+
+  @Test
+  public void testGetAZs() throws JsonProcessingException {
+    var data = new GetAZs("");
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::GetAZs\":\"\"}"));
+  }
+
+  @Test
+  public void testImportValue() throws JsonProcessingException {
+    var data = new ImportValue("sharedValueToImport");
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::ImportValue\":\"sharedValueToImport\"}"));
+  }
+
+  @Test
+  public void testJoin() throws JsonProcessingException {
+    var data = new Join(":", Arrays.asList("a", "b", "c"));
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::Join\":[\":\",[\"a\",\"b\",\"c\"]]}"));
+  }
+
+  @Test
+  public void testSelect() throws JsonProcessingException {
+    var data = new Select(1, Arrays.asList("apples", "grapes", "oranges", "mangoes"));
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::Select\":[1,[\"apples\",\"grapes\",\"oranges\",\"mangoes\"]]}"));
+  }
+
+  @Test
+  public void testSplit() throws JsonProcessingException {
+    var data = new Split("|", "a|b|c");
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Fn::Split\":[\"|\",\"a|b|c\"]}"));
+  }
+
+  @Test
+  public void testSub() throws JsonProcessingException {
+    {
+      var data = new Sub("String", Collections.emptyMap());
+      var actual = json.writeValueAsString(data);
+      assertThat(actual, is("{\"Fn::Sub\":\"String\"}"));
+    }
+    {
+      var variables = new HashMap<String, Object>();
+      variables.put("Var1Name", "Var1Value");
+      var data = new Sub("String", variables);
+
+      var actual = json.writeValueAsString(data);
+      assertThat(actual, is("{\"Fn::Sub\":[\"String\",{\"Var1Name\":\"Var1Value\"}]}"));
+    }
+  }
+
+  @Test
+  public void testTransform() throws JsonProcessingException {
+    var parameters = new HashMap<String, Object>();
+    parameters.put("key", "value");
+    var data = new Transform("macro_name", parameters);
+
+    var actual = json.writeValueAsString(data);
+    assertThat(
+        actual,
+        is("{\"Fn::Transform\":{\"Name\":\"macro_name\",\"Parameters\":{\"key\":\"value\"}}}"));
+  }
+
+  @Test
+  public void testRef() throws JsonProcessingException {
+    var data = new Ref("logicalName");
+    var actual = json.writeValueAsString(data);
+    assertThat(actual, is("{\"Ref\":\"logicalName\"}"));
+  }
+}
