afc1540f3879235ad3dee6344ce0ac0282618077
Shinya Tsuda <shinya@dacci.org>
2020-02-11 15:44:39 +0900

    Added YAML formatting options.

--- src/main/java/org/dacci/junk/yaml/FromJson.java
+++ src/main/java/org/dacci/junk/yaml/FromJson.java
@@ -1,60 +1,76 @@
 package org.dacci.junk.yaml;
 
-import static java.nio.charset.StandardCharsets.*;
-import static java.nio.file.StandardOpenOption.*;
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
-import org.dacci.junk.util.CloudFormationYaml;
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.DumperOptions.FlowStyle;
 import org.yaml.snakeyaml.Yaml;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.ParentCommand;
 
 @Command(name = "from-json")
 @Slf4j
 public class FromJson implements Runnable {
+  private static final Path HYPHEN_PATH = Paths.get("-");
   private static final ObjectMapper JSON = new ObjectMapper();
-  private static final DumperOptions DUMPER_OPTIONS;
 
-  static {
-    DUMPER_OPTIONS = new DumperOptions();
-    DUMPER_OPTIONS.setWidth(Integer.MAX_VALUE);
-    DUMPER_OPTIONS.setDefaultFlowStyle(FlowStyle.BLOCK);
-  }
+  @ParentCommand private YamlCommands parent;
 
-  @Option(names = "--cfn")
-  private boolean cfn;
+  @Option(names = "--charset")
+  private Charset charset = StandardCharsets.UTF_8;
 
-  @Parameters(arity = "1..")
-  private List<Path> files;
+  @Parameters private List<Path> files = new ArrayList<>();
 
   private Yaml yaml;
 
+  private Reader openForRead(Path file) throws IOException {
+    if (file.equals(HYPHEN_PATH)) {
+      return new BufferedReader(new InputStreamReader(System.in, charset));
+    } else {
+      return Files.newBufferedReader(file, charset);
+    }
+  }
+
+  private Writer openForWrite(Path file) throws IOException {
+    if (file.equals(HYPHEN_PATH)) {
+      return new BufferedWriter(new OutputStreamWriter(System.out, charset));
+    } else {
+      var yamlName = FilenameUtils.removeExtension(file.getFileName().toString()) + ".yaml";
+      return Files.newBufferedWriter(
+          file.resolveSibling(yamlName), charset, CREATE, TRUNCATE_EXISTING);
+    }
+  }
+
   private void processFile(Path file) {
     Object value;
-    try (var reader = Files.newBufferedReader(file, UTF_8)) {
+    try (var reader = openForRead(file)) {
       value = JSON.readValue(reader, Object.class);
     } catch (Exception e) {
       log.error("Failed to load", e);
       return;
     }
 
-    if (cfn) {
-      var dumped = yaml.dump(value);
-      value = yaml.load(dumped);
-    }
+    if (parent.isCloudFormation()) value = yaml.load(yaml.dump(value));
 
-    var yamlName = FilenameUtils.removeExtension(file.getFileName().toString()) + ".yaml";
-
-    try (var writer =
-        Files.newBufferedWriter(file.resolveSibling(yamlName), UTF_8, CREATE, TRUNCATE_EXISTING)) {
+    try (var writer = openForWrite(file)) {
       yaml.dump(value, writer);
     } catch (Exception e) {
       log.error("Failed to save", e);
@@ -64,8 +80,10 @@ public class FromJson implements Runnable {
 
   @Override
   public void run() {
-    yaml = cfn ? new CloudFormationYaml(DUMPER_OPTIONS) : new Yaml(DUMPER_OPTIONS);
+    yaml = parent.createYaml();
 
-    files.parallelStream().forEach(this::processFile);
+    if (files.isEmpty()) files.add(HYPHEN_PATH);
+
+    files.forEach(this::processFile);
   }
 }
--- src/main/java/org/dacci/junk/yaml/YamlCommands.java
+++ src/main/java/org/dacci/junk/yaml/YamlCommands.java
@@ -1,9 +1,96 @@
 package org.dacci.junk.yaml;
 
+import java.util.TimeZone;
+import lombok.Data;
 import org.dacci.junk.Main.CommandGroup;
+import org.dacci.junk.util.CloudFormationYaml;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.DumperOptions.FlowStyle;
+import org.yaml.snakeyaml.DumperOptions.LineBreak;
+import org.yaml.snakeyaml.DumperOptions.NonPrintableStyle;
+import org.yaml.snakeyaml.DumperOptions.ScalarStyle;
+import org.yaml.snakeyaml.DumperOptions.Version;
+import org.yaml.snakeyaml.Yaml;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 @Command(
     name = "yaml",
     subcommands = {FromJson.class})
-public class YamlCommands implements CommandGroup {}
+@Data
+public class YamlCommands implements CommandGroup {
+  @Option(names = "--disallow-unicode", description = "force ASCII characters only")
+  private boolean allowUnicode = true;
+
+  @Option(names = "--scalar-style", description = "default style for scalars")
+  private ScalarStyle scalarStyle = ScalarStyle.PLAIN;
+
+  @Option(names = "--indent", description = "indent width")
+  private int indent = 2;
+
+  @Option(names = "--indicator-indent", description = "indicator indent width")
+  private int indicatorIndent = 0;
+
+  @Option(names = "--version", description = "specification version")
+  private Version version = null;
+
+  @Option(names = "--canonical", description = "produce canonical YAML document")
+  private boolean canonical = false;
+
+  @Option(names = "--pretty-flow", description = "produce pretty flow YAML document")
+  private boolean prettyFlow = false;
+
+  @Option(names = "--width", description = "preferred width to emit scalars")
+  private int width = 80;
+
+  @Option(names = "--no-split-lines", description = "do not split lines exceeding preferred width")
+  private boolean splitLines = true;
+
+  @Option(names = "--flow-style", description = "the default flow style")
+  private FlowStyle flowStyle = FlowStyle.AUTO;
+
+  @Option(names = "--line-break", description = "line break to separate the lines")
+  private LineBreak lineBreak = LineBreak.UNIX;
+
+  @Option(names = "--explicit-start")
+  private boolean explicitStart = false;
+
+  @Option(names = "--explicit-end")
+  private boolean explicitEnd = false;
+
+  @Option(names = "--time-zone")
+  private TimeZone timeZone = null;
+
+  @Option(
+      names = "--max-simple-key-length",
+      description = "max key length to use simple key (without '?')")
+  private int maxSimpleKeyLength = 128;
+
+  @Option(names = "--non-printable-style")
+  private NonPrintableStyle nonPrintableStyle = NonPrintableStyle.BINARY;
+
+  @Option(names = "--cfn", description = "handle CloudFormation intrinsics")
+  private boolean cloudFormation = false;
+
+  public Yaml createYaml() {
+    var dumperOptions = new DumperOptions();
+    dumperOptions.setAllowUnicode(allowUnicode);
+    dumperOptions.setDefaultScalarStyle(scalarStyle);
+    dumperOptions.setIndent(indent);
+    dumperOptions.setIndicatorIndent(indicatorIndent);
+    dumperOptions.setVersion(version);
+    dumperOptions.setCanonical(canonical);
+    dumperOptions.setPrettyFlow(prettyFlow);
+    dumperOptions.setWidth(width < 0 ? Integer.MAX_VALUE : width);
+    dumperOptions.setSplitLines(splitLines);
+    dumperOptions.setDefaultFlowStyle(flowStyle);
+    dumperOptions.setLineBreak(lineBreak);
+    dumperOptions.setExplicitStart(explicitStart);
+    dumperOptions.setExplicitEnd(explicitEnd);
+    dumperOptions.setTimeZone(timeZone);
+    dumperOptions.setMaxSimpleKeyLength(maxSimpleKeyLength);
+    dumperOptions.setNonPrintableStyle(nonPrintableStyle);
+
+    return cloudFormation ? new CloudFormationYaml(dumperOptions) : new Yaml(dumperOptions);
+  }
+}
