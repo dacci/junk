200e5086e2e3dd5cd9a4a3a4c5e58b467a4503a1
Shinya Tsuda <shinya@dacci.org>
2021-07-10 11:28:26 +0900

    Simplified json sort-config.

--- src/main/java/org/dacci/junk/json/SortConfig.java
+++ src/main/java/org/dacci/junk/json/SortConfig.java
@@ -1,7 +1,6 @@
 package org.dacci.junk.json;
 
 import static java.nio.charset.StandardCharsets.*;
-import static java.nio.file.StandardOpenOption.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,9 +9,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 import org.dacci.junk.util.JsonStringifyPrettyPrinter;
 
@@ -23,15 +22,21 @@ import picocli.CommandLine.Parameters;
 @Command(name = "sort-config")
 @Slf4j
 public class SortConfig implements Runnable {
+  public static class Config extends TreeMap<String, Object> {
+    public Config() {
+      super(SortConfig::keyComparator);
+    }
+  }
+
   private static final ObjectMapper JSON =
       new ObjectMapper()
-          .setDefaultPrettyPrinter(JsonStringifyPrettyPrinter.builder().indent("    ").build())
-          .enable(SerializationFeature.INDENT_OUTPUT);
-  private static TypeReference<Map<String, Object>> CONFIG_TYPE =
-      new TypeReference<Map<String, Object>>() {};
+          .setDefaultPrettyPrinter(
+              JsonStringifyPrettyPrinter.builder().lineSeparator("\n").indent("    ").build())
+          .enable(SerializationFeature.INDENT_OUTPUT)
+          .registerModule(new SimpleModule().addAbstractTypeMapping(Map.class, TreeMap.class));
 
   private static int keyComparator(String a, String b) {
-    if (a == null && b == null) return 0;
+    if (a == b) return 0;
     if (a == null) return -1;
     if (b == null) return 1;
 
@@ -41,19 +46,16 @@ public class SortConfig implements Runnable {
   }
 
   private static void process(Path file) {
-    Map<String, Object> loaded;
-    try (var reader = Files.newBufferedReader(file)) {
-      loaded = JSON.readValue(reader, CONFIG_TYPE);
+    Config config;
+    try (var reader = Files.newBufferedReader(file, UTF_8)) {
+      config = JSON.readValue(reader, Config.class);
     } catch (IOException e) {
       log.error("Failed to load", e);
       return;
     }
 
-    var sorted = new TreeMap<>(SortConfig::keyComparator);
-    sorted.putAll(loaded);
-
-    try (var writer = Files.newBufferedWriter(file, UTF_8, CREATE, TRUNCATE_EXISTING)) {
-      JSON.writeValue(writer, sorted);
+    try (var writer = Files.newBufferedWriter(file, UTF_8)) {
+      JSON.writeValue(writer, config);
     } catch (IOException e) {
       log.error("Failed to save", e);
       return;
